eb08f359ddd2b36d0fc29fd309cbc7081598da47
Gabriel Alcântara <galcantaradev@gmail.com>
2022-04-17 18:43:41 -0300

    update stories ts

--- boilerplate/src/components/Main/stories.tsx
+++ boilerplate/src/components/Main/stories.tsx
@@ -1,13 +1,15 @@
-import Main from './index';
+import { Meta, Story } from '@storybook/react/types-6-0';
 import { withKnobs, text } from '@storybook/addon-knobs';
+
+import Main from './index';
 
 export default {
   title: 'Main',
   component: Main,
   decorators: [withKnobs]
-};
+} as Meta;
 
-export const Basic = () => (
+export const Basic: Story = () => (
   <Main
     title={text('Title', 'React Avançado')}
     description={text(
