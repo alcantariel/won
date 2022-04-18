db61b18fbac10f98db095bd407f787165edfaf76
Gabriel Alcântara <galcantaradev@gmail.com>
2022-04-18 18:26:15 -0300

    add next-pwa

--- boilerplate/next.config.js
+++ boilerplate/next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  ...withPWA({
+    pwa: {
+      dest: 'public',
+      disable: !isProduction
+    }
+  })
 };
 
 module.exports = nextConfig;
